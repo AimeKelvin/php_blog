a6d574e1f38ac65ac4999e40242eddaee7197231
Shimwa Aime Kelvin <158560776+AimeKelvin@users.noreply.github.com>
2024-12-31 20:57:04 +0200

    setting up footer

--- admin/create.php
+++ admin/create.php
@@ -1,37 +1,32 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../assets/globals.css">
-    <title>Dashboard</title>
-</head>
-<body>
+<?php 
+include('../components/header.php');
+?>
     <div class="dashboard">
         <div class="sidebar">
-
+<h3 class="admin-title">Aurablog</h3>
         </div>
         <div class="create-form">
             <form action="" method="post">
                 <div class="form-field">
-                    <input type="text" name="title" id="">
+                    <input type="text" name="title" id="" placeholder="Title">
                 </div>
 
                 <div class="form-field">
-                    <textarea name="summary" rows="10" cols="30" id="" placeholder="Enter Summary"></textarea>
+                    <textarea name="summary" rows="10" cols="30" id="" placeholder="Summary"></textarea>
                 </div>
 
                 <div class="form-field">
-                    <textarea name="content" rows="10" cols="30" id="" placeholder="What do want to talk about"></textarea>
+                    <textarea name="content" rows="10" cols="30" id="" placeholder="The full cup of tea"></textarea>
                 </div>
                 <input type="hidden" name="date" value="<?php echo date("y/m/d"); ?>">
 
                 <div class="form-field">
                     <input type="submit" value="Post" name="create">
                 </div>
-
             </form>
         </div>
     </div>
-</body>
-</html>
+
+    <?php 
+include('../components/footer.php');
+?>
